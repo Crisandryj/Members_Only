9cbd8702455cd07e1df7d58d750eeb2e943f948a
crisandryj <crisandryj@gmail.com>
2022-10-05 17:52:27 -0400

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  end
-  
-  root to: "home#index"
+devise_for :users, controllers: { sessions: 'users/sessions' }
+
+
+authenticated do
+  root :to => 'dashboard#home'
+end
+
+unauthenticated do
+  root :to => 'home#index'
+end
+
 end
